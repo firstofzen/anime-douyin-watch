63ec0d1994e99aa51130fa46614d2170aa46a6f6
ducanhTop1global <firstofzen@gmail.com>
2022-03-15 14:55:51 -0700

    ducanhTop1Global

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,4 +1,3 @@
-
 import {
     Avatar,
     Box,
@@ -27,7 +26,7 @@ import axios from "axios";
 import './UserInfo.css';
 import React, {useState} from "react";
 import Video from "./Video";
-import { Forum, Notifications, SupervisorAccount, CancelPresentation, GroupAdd, PersonAdd } from '@material-ui/icons';
+import {Forum, Notifications, SupervisorAccount, CancelPresentation, GroupAdd, PersonAdd} from '@material-ui/icons';
 import DialogContext from "@mui/material/Dialog/DialogContext";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -41,24 +40,24 @@ export default function UserInfo() {
     const [listFr, setListFr] = useState([]);
     const [listQueueFr, setListQueueFr] = useState([]);
     const [openDialogSearchFr, setOpenDialogSearchFr] = useState(false);
-    const [textSearchFr,setTextSearchFr] =useState("");
+    const [textSearchFr, setTextSearchFr] = useState("");
     const [resltSearchFr, setResltSearchFr] = useState([]);
     React.useEffect(() => {
-        axios.get("https://anime-douyin.herokuapp.com/getAllVideoLiked", { params: { email: params.get('email') }}).then(resp => {
+        axios.get("https://anime-douyin.herokuapp.com/getAllVideoLiked", {params: {email: params.get('email')}}).then(resp => {
             console.log(resp.data)
             setVidLiked(resp.data)
         }).catch(er => console.log(er));
     }, []);
 
     const handleOpenDialogFr = () => {
-        axios.get("https://anime-douyin.herokuapp.com/getListFr", {params:{email:params.get('email')}})
+        axios.get("https://anime-douyin.herokuapp.com/getListFr", {params: {email: params.get('email')}})
             .then(resp => setListFr(resp.data))
             .catch(er => console.log(er))
             .finally(() => setOpenDialogFr(true));
     }
 
     const handleOpenDialogQueueFr = () => {
-        axios.get("https://anime-douyin.herokuapp.com/getQueueFr", {params:{email:params.get('email')}}).then(resp => {
+        axios.get("https://anime-douyin.herokuapp.com/getQueueFr", {params: {email: params.get('email')}}).then(resp => {
             setListQueueFr(resp.data);
         }).catch(er => console.log(er)).finally(() => setOpenDialogQueueFr(true))
     }
@@ -66,20 +65,23 @@ export default function UserInfo() {
 
     return (
         <Container maxWidth="lg">
-            <Box sx={{ bgcolor: '#263238', height: '45vh' }}>
+            <Box sx={{bgcolor: '#263238', height: '45vh'}}>
                 <div style={{
                     padding: 20, marginTop: 16, marginLeft: 45, display: 'flex',
                     flexDirection: 'column',
                     alignItems: 'center',
                 }}>
-                    <Avatar sx={{ width: 80, height: 80, m: 3, marginTop: 4 }} src={params.get('image')}/>
+                    <Avatar sx={{width: 80, height: 80, m: 3, marginTop: 4}} src={params.get('image')}/>
                     <Typography component="h1" variant="h4">{params.get('name')}</Typography>
-                    <Grid sx={{ flexGrow: 1, margin:3, width: 'auto', height: 'auto'}} container justifyContent="center" direction="row" alignItems="center">
-                        <Grid item><IconButton ><Notifications fontSize={'large'} /></IconButton></Grid>
-                        <Grid item><IconButton ><Forum fontSize={'large'}/></IconButton></Grid>
+                    <Grid sx={{flexGrow: 1, margin: 3, width: 'auto', height: 'auto'}} container justifyContent="center"
+                          direction="row" alignItems="center">
+                        <Grid item><IconButton><Notifications fontSize={'large'}/></IconButton></Grid>
+                        <Grid item><IconButton><Forum fontSize={'large'}/></IconButton></Grid>
                         <Grid item>
-                            <IconButton onClick={handleOpenDialogFr}><SupervisorAccount fontSize={'large'}/></IconButton>
-                            <Dialog TransitionComponent={Transition} fullScreen onClose={() => setOpenDialogFr(false)} open={openDialogFr}>
+                            <IconButton onClick={handleOpenDialogFr}><SupervisorAccount
+                                fontSize={'large'}/></IconButton>
+                            <Dialog TransitionComponent={Transition} fullScreen onClose={() => setOpenDialogFr(false)}
+                                    open={openDialogFr}>
                                 <AppBar sx={{position: 'relative'}}>
                                     <Toolbar>
                                         <IconButton
@@ -88,10 +90,10 @@ export default function UserInfo() {
                                             onClick={() => setOpenDialogFr(false)}
                                             aria-label="close"
                                         >
-                                            <CancelPresentation />
+                                            <CancelPresentation/>
                                         </IconButton>
                                         <IconButton edge={'end'} onClick={handleOpenDialogQueueFr}>
-                                            <GroupAdd />
+                                            <GroupAdd/>
                                         </IconButton>
                                         <Dialog open={openDialogQueueFr} onClose={() => setOpenDialogQueueFr(false)}>
                                             <DialogContent>
@@ -100,7 +102,7 @@ export default function UserInfo() {
                                                         listQueueFr.map(({image, name}) =>
                                                             <ListItem>
                                                                 <ListItemAvatar>
-                                                                    <Avatar src={image} />
+                                                                    <Avatar src={image}/>
                                                                 </ListItemAvatar>
                                                                 <ListItemText primary={name}/>
                                                             </ListItem>
@@ -116,7 +118,7 @@ export default function UserInfo() {
                                         listFr.map(({image, name}) =>
                                             <ListItem>
                                                 <ListItemAvatar>
-                                                    <Avatar src={image} />
+                                                    <Avatar src={image}/>
                                                 </ListItemAvatar>
                                                 <ListItemText primary={name}/>
                                             </ListItem>
@@ -127,46 +129,47 @@ export default function UserInfo() {
                         </Grid>
                         <Grid>
                             <IconButton onClick={() => setOpenDialogSearchFr(true)}>
-                                <PersonAdd />
+                                <PersonAdd/>
                             </IconButton>
                             <Dialog open={openDialogSearchFr} onClose={() => setOpenDialogSearchFr(false)}>
                                 <DialogTitle>Search User</DialogTitle>
-                                <TextField  hiddenLabel
-                                            id="filled-hidden-label-normal"
-                                            variant="filled"
-                                            size={'medium'}
-                                            onChange={ev => setTextSearchFr(ev.target.value)}
+                                <TextField hiddenLabel
+                                           id="filled-hidden-label-normal"
+                                           variant="filled"
+                                           size={'medium'}
+                                           onChange={ev => setTextSearchFr(ev.target.value)}
                                 />
+                                <List>
+                                    {
+                                        resltSearchFr.map(({image, name, email}) =>
+                                            <ListItem>
+                                                <ListItemAvatar src={image}/>
+                                                <ListItemText primary={name}/>
+                                            </ListItem>
+                                        )
+                                    }
+                                </List>
                                 <DialogActions><Button onClick={() => {
-                                    axios.get("https://anime-douyin.herokuapp.com/searchUsrByPrefixName", {params:{prefixNameFr:textSearchFr}})
+                                    axios.get("https://anime-douyin.herokuapp.com/searchUsrByPrefixName", {params: {prefixNameFr: textSearchFr}})
                                         .then(resp => setResltSearchFr(resp.data))
                                         .catch(er => console.log(er));
                                 }}>Search</Button></DialogActions>
-                                <DialogContext>
-                                    <List>
-                                        {
-                                            resltSearchFr.map(({image, name, email}) =>
-                                                <ListItem>
-                                                    <ListItemAvatar src={image} />
-                                                    <ListItemText primary={name}/>
-                                                </ListItem>
-                                            )
-                                        }
-                                    </List>
-                                </DialogContext>
+
                             </Dialog>
                         </Grid>
                     </Grid>
                 </div>
             </Box>
-            <Box sx={{ bgcolor: '#202c2f', height: '70vh' }}>
-            <Chip size='large' label="List Video You Liked :3" color="success" avatar={<Avatar src={params.get('image')} />} /> 
+            <Box sx={{bgcolor: '#202c2f', height: '70vh'}}>
+                <Chip size='large' label="List Video You Liked :3" color="success"
+                      avatar={<Avatar src={params.get('image')}/>}/>
                 <div className='vidLiked'>
                     <div className='app'>
                         <div className='app_video'>
                             {
                                 listVidLiked.map(({url, comments, amountLike, asset_id, listUserLiked}) =>
-                                    <Video url={url} comments={comments} id_vid={asset_id} listUserLiked={listUserLiked} amountLike={amountLike}/>
+                                    <Video url={url} comments={comments} id_vid={asset_id} listUserLiked={listUserLiked}
+                                           amountLike={amountLike}/>
                                 )
                             }
                         </div>
